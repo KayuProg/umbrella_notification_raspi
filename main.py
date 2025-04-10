3de50f7f53ed06bf8affc8bf479d62bb3d0e72f7
kayuprog <kayuprog@gmail.com>
2025-04-10 20:20:48 +0900

    fix

--- main.py
+++ main.py
@@ -49,7 +49,8 @@ def pico_connect():
         # from playsound3 import playsound        
         #playsound("./audio/notice.mp3")
         #pygame.mixer.music.set_volume(1)
-        os.system("/usr/bin/mplayer -speed 1.1 -volume 100 -af scaletempo /home/kayu/Desktop/umbrella_notification_raspi/audio/notice.mp3")
+        #os.system("amixer sset Master 100%+")
+        os.system("/usr/bin/mplayer -speed 1.1 -volume 90 -af scaletempo /home/kayu/Desktop/umbrella_notification_raspi/audio/notice.mp3")
     
         flag=1
 
@@ -93,7 +94,7 @@ def read_aloud():
     # from playsound3 import playsound        
     # playsound(r"audio/readaloud.mp3")
     #pygame.mixer.music.set_volume(0.1)
-    os.system("/usr/bin/mplayer -speed 1.1 -volume 100 -af scaletempo /home/kayu/Desktop/umbrella_notification_raspi/audio/readaloud.mp3")
+    os.system("/usr/bin/mplayer -speed 1.1 -volume 95 -af scaletempo /home/kayu/Desktop/umbrella_notification_raspi/audio/readaloud.mp3")
   
 
     # # mixerモジュールの初期化
